9906cc66ccf3141238feef8b512db96ed079151c
ngaraug <gaurangbmestry3@gmail.com>
2023-07-18 20:03:41 +0530

    Port for deployment add

--- index.js
+++ index.js
@@ -95,4 +95,4 @@ app.get('/:customListName', (req, res)=>{
     console.log(req.params.customListName)
 })
 
-app.listen(3000, ()=>console.log("Listening on port 3000"))
+app.listen(process.env.PORT || 3000, ()=>console.log("Listening on port 3000"))
